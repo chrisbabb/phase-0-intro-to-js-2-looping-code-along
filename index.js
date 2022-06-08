728a856ca6d4fbb9398b1c347024e415e6f4180c
Christopher Babb <chris@appsoftdev.com>
2022-06-08 14:29:36 -0700

    commit tree 914c33fb622209b725779bc183e1877640971586

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(myArray, message) {
+    let returnedArray = [];
+    for(let i=0;i<myArray.length;++i){
+        returnedArray[i] = `Thank you, ${myArray[i]}, for the wonderful ${message} gift!`
+    }
+    return returnedArray
+}
+
+function countDown(number) {
+    for(let i=number;i>-1;--i){
+        console.log(i)
+    }
+}
